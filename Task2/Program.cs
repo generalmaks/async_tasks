f6b1ce0c915523c4931ef79316825c94651ae29d
generalmaks <schrinan@gmail.com>
2024-12-28 17:12:56 +0200

    Task 2 created

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Func<string, Task<string>> func = (item) =>
+        {
+            Task<string> result = Task.Run(() =>
+            {
+                if (item.Length > 10)
+                    return item.Substring(0, 10);
+                throw new Exception($"Bad item: {item}");
+            });
+            return result;
+        };
+        
+    }
+
+    static Task<T[]> MapAsync<T>(T[] array, Func<T, Task<T>> func)
+    {
+        var t = Task.Run(() =>
+        {
+            int length = array.Length;
+            T[] results = new T[length];
+            Task[] tasks = new Task[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = i;
+                tasks[index] = func(array[index]).ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                        throw task.Exception.GetBaseException();
+
+                    results[index] = task.Result;
+                });
+            }
+
+            Task.WaitAll(tasks);
+            return results;
+        });
+        return t;
     }
 }
--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d57411a559283e14674e3fa9ca8740b12ad795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff09bfbc3134e327767b3b5739e5d48a7ab88228")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
