9e1b712f8b176b63cae473d49186469cdb313a47
generalmaks <schrinan@gmail.com>
2024-12-28 17:29:27 +0200

    Added another example

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,7 +14,66 @@ class Program
             });
             return result;
         };
-        
+        var strings = new string[]
+        {
+            "Hello World!",
+            "Goodbye World!",
+            "See you again!",
+            "Goodbye friend!",
+            "Hello again!",
+            "See you tomorrow!",
+        };
+        var finalTask = MapAsync(strings, func);
+
+        finalTask.ContinueWith(result =>
+        {
+            if (result.IsFaulted)
+            {
+                var a = result.Exception.Flatten().InnerExceptions;
+                foreach (var ex in a)
+                    Console.WriteLine(ex.GetBaseException().Message);
+            }
+            else
+                foreach (var item in result.Result)
+                {
+                    Console.WriteLine($"Result: {item}");
+                }
+        }).Wait();
+
+        Func<int, Task<int>> func2 = (number) =>
+        {
+            Task<int> result = Task.Run(() =>
+            {
+                int delayNumber = number * 100;
+                Task.Delay(delayNumber);
+                if (number % 2 == 0)
+                    return number * 2;
+                else
+                    return number;
+            });
+            return result;
+        };
+
+        var numbers = new int[25];
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = i;
+        var finalTask2 = MapAsync(numbers, func2);
+        finalTask2.ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Console.WriteLine($"Task failed: {task.Exception?.GetBaseException().Message}");
+            }
+            else
+            {
+                // Output the results
+                foreach (var result in task.Result)
+                {
+                    Console.WriteLine($"Result: {result}");
+                }
+            }
+        });
+        Console.ReadLine();
     }
 
     static Task<T[]> MapAsync<T>(T[] array, Func<T, Task<T>> func)
