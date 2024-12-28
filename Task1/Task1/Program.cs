3f577649cdd39c2db91f081ca3064aa0a40f1648
generalmaks <schrinan@gmail.com>
2024-12-28 15:41:30 +0200

    Done first task

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
     }
+
+
+    static void MapAsync<T>(T[] array, Action<T, Action<ItemInfo<T>>> asyncCallback,
+        Action<Exception[], T[]> callbackFinal)
+    {
+        int arrayLength = array.Length;
+
+        List<T> results = new List<T>();
+        List<Exception> errors = new List<Exception>();
+
+        int counter = 0;
+
+        object locker = new object();
+
+        foreach (T item in array)
+        {
+            asyncCallback(item, (cortege) =>
+            {
+                lock (locker)
+                {
+                    if (cortege.Error is not null)
+                    {
+                        errors.Add(cortege.Error);
+                    }
+                    else
+                    {
+                        results.Add(cortege.Value);
+                    }
+
+                    counter++;
+
+                    if (counter == arrayLength)
+                        callbackFinal(errors.ToArray(), results.ToArray());
+                }
+            });
+        }
+    }
+}
+
+public struct ItemInfo<T>
+{
+    public ItemInfo(Exception error)
+    {
+        Error = error;
+        Value = default;
+    }
+
+    public ItemInfo(T value)
+    {
+        Error = null;
+        Value = value;
+    }
+
+    public Exception Error { get; }
+    public T Value { get; }
 }
--- Task1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Task1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f78b0f3d85110b416246b46c25602d0d64d8fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
