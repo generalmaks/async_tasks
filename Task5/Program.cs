ced78f4902a0168e8c75fcfbfd7af3c47cf6db18
generalmaks <schrinan@gmail.com>
2024-12-31 17:08:07 +0200

    Made task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+class Program
+{
+    static void Main()
+    {
+        var subject = new Subject<string>();
+
+        var subscription1 = subject.Subscribe(
+            message => Console.WriteLine($"Subscriber 1 received: {message}"),
+            ex => Console.WriteLine($"Subscriber 1 encountered an error: {ex.Message}"),
+            () => Console.WriteLine("Subscriber 1 completed")
+        );
+
+        var subscription2 = subject.Subscribe(
+            message => Console.WriteLine($"Subscriber 2 received: {message}"),
+            ex => Console.WriteLine($"Subscriber 2 encountered an error: {ex.Message}"),
+            () => Console.WriteLine("Subscriber 2 completed")
+        );
+
+        subject.OnNext("Hello, Reactive Extensions!");
+        subject.OnNext("Learning Rx in C#");
+
+        subject.OnCompleted();
+
+        subscription1.Dispose();
+        subscription2.Dispose();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
